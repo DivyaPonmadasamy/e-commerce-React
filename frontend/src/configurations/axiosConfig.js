07a98b23720f52a6324f84c2765620aa233507bc
Divya Ponmadasamy <pdwin9@live.com>
2026-01-20 11:52:34 +0530

    Add axios config to use production URL

--- frontend/src/configurations/axiosConfig.js
+++ frontend/src/configurations/axiosConfig.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 export const axiosAuth = axios.create({
-  baseURL: "http://localhost:8080",
+  // baseURL: "http://localhost:8080",
+  baseURL: process.env.REACT_APP_API_BASE_URL,  // React only exposes REACT_APP variables to the code
   withCredentials: true,
 });
 
 export const axiosPublic = axios.create({
-  baseURL: "http://localhost:8080",
+  // baseURL: "http://localhost:8080",
+  baseURL: process.env.REACT_APP_API_BASE_URL,
 });
